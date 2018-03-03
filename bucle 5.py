52e60b1fc9ff893bf52fe612bcbf24f09cb1a194
jordi1324 <35691364+jordi1324@users.noreply.github.com>
2018-03-03 09:42:46 +0100

    Delete bucle 5.py

--- bucle 5.py
+++
@@ -1,9 +0,0 @@
-pal=int(input("Escribe cuantas palabras quieres que tenga la lista: "))
-lista=[]
-rem=0
-for n in range(0,pal):
-        print(f"Digame la palabra {n+1}")
-        palabra=input()
-        lista+=[palabra]
-
-print(f"La lista es {lista}")
